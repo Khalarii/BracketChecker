8bf61af38986fa0cd0cd5943f68d7697780da0b3
Kim Souza <kimgss94@hotmail.com>
2020-07-08 18:50:26 -0300

    Initial commit

---
+++ BracketChecker/CheckBrackets.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketChecker
+{
+    public static class CheckBrackets
+    {
+        /// <summary>
+        /// Checks if input string has correct bracket opening/closing
+        /// </summary>
+        /// <returns>True for correct bracket input and false for incorrect bracket input</returns>
+        public static bool IsBalanced(string s)
+        {
+            Dictionary<char, char> brackets = new Dictionary<char, char> { { '{', '}' }, { '[', ']' }, { '(', ')' } };
+            //List of all open brackets in order to check against any closing brackets
+            List<char> openBrackets = new List<char>();
+
+            foreach (var c in s)
+            {
+                if (brackets.ContainsKey(c))
+                {
+                    openBrackets.Add(c);
+                }
+                else if (openBrackets.Any() && c == brackets[openBrackets.Last()])
+                {
+                    //Opening bracket has correct closing bracket so remove it from list
+                    openBrackets.RemoveAt(openBrackets.Count - 1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            //Open bracket without closing bracket in string
+            if (openBrackets.Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BracketChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> BracketList = new List<string>
+            {
+                "{}", "{[]}", "({([])})", "{[])", "{{{", "{)"
+            };
+
+            foreach (var bracket in BracketList)
+            {
+                Console.WriteLine(string.Concat(bracket, ": ", CheckBrackets.IsBalanced(bracket) ? "CORRECT" : "INCORRECT"));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
